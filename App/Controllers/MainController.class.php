2b8af9ad0b9d7148847f78bdb48a6623fd507686
courgette <courgette@potiron>
2017-12-04 13:01:32 +0100

    add alpha, alphanum, count of uppercases & digits in formvalidator

--- App/Controllers/MainController.class.php
+++ App/Controllers/MainController.class.php
@@ -24,6 +24,14 @@ class MainController extends AppController{
     $fields_format = ["test_form_min" => [ "min" => 3 ],
                       "test_form_max" => [ "max" => 4 ],
                       "test_form_min_max" => [  "min" => 3, "max" => 6 ],
+                      "test_optionnal" => [  "optionnal" => true, "min" => 3 ],
+                      "test_exact_size" => [ "size" => 2],
+                      "test_optionnalif" => [ "optionnalif" => ["test_exact_size" => ["US"]], "size" => 5],
+                      "test_alpha" => ["alpha" => true],
+                      "test_alphanum" => ["alphanum" => true],
+                      "test_numeric" => ["numeric" => true],
+                      "test_int" => ["int" => true],
+                      "test_secure" => ["minuppercases" => 2, "mindigits" => 3,  "Maxnbspaces" => 0],
                       ];
     if(!$fv->formSubmitted())
       $this->addMessage("Form not sumbitted", "info", "user");
@@ -40,6 +48,14 @@ class MainController extends AppController{
     \Core\Template::setStatic("test_form_min", $fv->getFieldValueSecure("test_form_min", ""));
     \Core\Template::setStatic("test_form_max", $fv->getFieldValueSecure("test_form_max", ""));
     \Core\Template::setStatic("test_form_min_max", $fv->getFieldValueSecure("test_form_min_max", ""));
+    \Core\Template::setStatic("test_optionnal", $fv->getFieldValueSecure("test_optionnal", ""));
+    \Core\Template::setStatic("test_exact_size", $fv->getFieldValueSecure("test_exact_size", ""));
+    \Core\Template::setStatic("test_optionnalif", $fv->getFieldValueSecure("test_optionnalif", ""));
+    \Core\Template::setStatic("test_alpha", $fv->getFieldValueSecure("test_alpha", ""));
+    \Core\Template::setStatic("test_alphanum", $fv->getFieldValueSecure("test_alphanum", ""));
+    \Core\Template::setStatic("test_numeric", $fv->getFieldValueSecure("test_numeric", ""));
+    \Core\Template::setStatic("test_int", $fv->getFieldValueSecure("test_int", ""));
+    \Core\Template::setStatic("test_secure", $fv->getFieldValueSecure("test_secure", ""));
 
     //render in template
     \Core\Template::setStatic("content", \Core\Template::render("pages/main/form_test.html"));
--- Core/FormValidator.class.php
+++ Core/FormValidator.class.php
@@ -45,21 +45,50 @@ class FormValidator{
 
     /* function to check field format (see header for fofmat detail) */
     public function checkFieldFormat($field, $format){
-        if($this->fieldSubmitted($field) && $this->getFieldValue($field) != null){
+        if(!$this->fieldIsEmpty($field)){
           foreach($format as $type => $value){
-            $check_method = "check".ucfirst(strtolower($type));
-            if(method_exists($this, $check_method)){
-              //$this->AddMessage("Check ".$type." on field ".$field.".", "info", "dev");
-              return $this->$check_method($field, $value);
+            $valid = true;
+            if(strtolower($type) != "optionnal" && strtolower($type) != "optionnalif"){
+              $check_method = "check".ucfirst(strtolower($type));
+              if(!method_exists($this, $check_method) || $this->$check_method($field, $value)){
+                $valid = false;
+              }
             }
-            else
-              $this->AddMessage("Cannot check ".$type." on field ".$field.".", "error", "dev");
           }
+          return $valid;
         }
+        else if ($this->fieldIsOptionnal($field, $format) && $this->fieldIsEmpty($field))
+          return true;
         else
             $this->AddMessage("Field ".$field." empty or impossible to check.", "error", "user");
 
         return false;
+    }
+
+    protected function fieldIsOptionnal($field, $format){
+      if(isset($format["optionnal"]))
+        return true;
+      elseif(isset($format["optionnalif"]) && is_array($format["optionnalif"])){
+        $valid = true;
+        foreach($format["optionnalif"] as $field_to_check => $values_to_check){
+          if(is_array($values_to_check)){
+            if(!$this->fieldSubmitted($field_to_check) || in_array($this->getFieldValue($field_to_check), $values_to_check)){
+              $this->addMessage("Field ".$field." is not optionnal");
+              return false;
+            }
+          }
+        }
+        return $valid;
+      }
+      //$this->addMessage("Field ".$field." is not optionnal");
+      return false;
+    }
+
+    protected function fieldIsEmpty($field){
+      if(isset($this->_data[$field]) && (empty($this->_data[$field]) || strlen($this->_data[$field]) <= 0))
+        return true;
+      //$this->addMessage("Field ".$field." is not empty(".$this->data[$field].")".strlen($this->data[$field]).".", "info", "dev");
+      return false;
     }
 
     /* function to check field format on multiple fields */
@@ -76,6 +105,15 @@ class FormValidator{
     ************  CHECK STRING SIZE **********
     ******************************************/
     /* function to check min size of field */
+    protected function checkSize($field, $size){
+        if(strlen($this->getFieldValue($field)) == $size )
+            return true;
+        else if($this->loggerDefined())
+            $this->AddMessage("Field ".$field. " must have a size of ".$size." characters.", "error", "user");
+        return false;
+    }
+
+    /* function to check min size of field */
     protected function checkMin($field, $min){
         if(strlen($this->getFieldValue($field)) >= $min )
             return true;
@@ -86,7 +124,7 @@ class FormValidator{
 
     /* function to check max size of field */
     protected function checkMax($field, $max){
-        if(strlen($this->getFieldValue($field)) <= $max )
+        if(!$this->fieldIsEmpty($field) && strlen($this->getFieldValue($field)) <= $max )
             return true;
         else if($this->loggerDefined())
             $this->AddMessage("Field ".$field. " must have a max length of ".$max." characters.","error", "user");
@@ -97,6 +135,43 @@ class FormValidator{
     /******************************************
     ************  CHECK FIELD TYPE ***********
     ******************************************/
+    /* check if field is only alphabetic */
+    protected function checkAlpha($field, $value){
+      if(ctype_alpha($this->getFieldValue($field)))
+        return true;
+        else if($this->loggerDefined())
+            $this->addMessage("Field ".$field." (".$this->getFieldValue($field).") has to be alphabetic.", "error", "user");
+      return false;
+    }
+
+    /* check if field is only alphanumeric */
+    protected function checkAlphanum($field, $value){
+      if(ctype_alnum($this->getFieldValue($field)))
+        return true;
+      else if($this->loggerDefined())
+          $this->addMessage("Field ".$field." (".$this->getFieldValue($field).") has to be alphanumeric.", "error", "user");
+      return false;
+    }
+
+    /* check if field is only alphanumeric */
+    protected function checkNumeric($field, $value = true){
+        if(is_numeric($this->getFieldValue($field)))
+          return true;
+        else if($this->loggerDefined())
+          $this->addMessage("Field ".$field." (".$this->getFieldValue($field).") has to be numeric.", "error", "user");
+      return false;
+    }
+
+
+    /* check if value is integer */
+    protected function checkInt($field, $value = true){
+        if(is_numeric($this->getFieldValue($field)) && is_int(intval($this->getFieldValue($field))))
+          return true;
+        else if($this->loggerDefined())
+          $this->addMessage("Field ".$field." (".$this->getFieldValue($field).") has to be an integer.", "error", "user");
+      return false;
+    }
+
     /* function to check that field value has an email format */
     protected function checkEmail($field, $value = true){
         if(filter_var($this->getFieldValue($field), FILTER_VALIDATE_EMAIL))
@@ -115,14 +190,33 @@ class FormValidator{
         return false;
     }
 
-    /* check if value is integer */
-    protected function checkInt($field, $value = true){
-        if(is_numeric($this->getFieldValue($field)) && is_int(intval($this->getFieldValue($field))))
+    /* check number of uppercases letters in string */
+    protected function checkMinuppercases($field, $nb_uppercase_required){
+        if(preg_match_all('/[A-Z]/', $this->getFieldValue($field)) >= $nb_uppercase_required)
+         return true;
+        else if($this->loggerDefined())
+           $this->AddMessage("Field ".$field. " must contain at least".$nb_uppercase_required." upppercases character ".preg_match_all('/[A-Z]/', $this->getFieldValue($field))." given.", "error", "user");
+        return false;
+    }
+
+    /* check number of digits in string */
+    protected function checkMindigits($field, $nb_digits_required){
+        if(preg_match_all('/[0-9]/', $this->getFieldValue($field)) >= $nb_digits_required)
           return true;
         else if($this->loggerDefined())
-          $this->addMessage("Field ".$field." (".$this->getFieldValue($field).") is not an integer.", "error", "user");
+          $this->AddMessage("Field ".$field. " must contain at least".$nb_digits_required." digits.", "error", "user");
+        return false;
+    }
+
+    /* check number of spaces in string */
+    protected function checkMaxnbspaces($field, $nb_allowed_spaces){
+      if(substr_count($this->getFieldValue($field), ' ') <= $nb_allowed_spaces)
+        return true;
+      else if($this->loggerDefined())
+           $this->AddMessage("Not more than ".$nb_allowed_spaces. "  spaces are allowed in ".$field.".", "error", "user");
       return false;
     }
+
 
     /******************************************
     ************  CHECK DATE FUNCTIONS *******
@@ -239,7 +333,6 @@ class FormValidator{
             if(isset($this->_data[$f]))
               return $this->_data[$f];
           }
-
         return $default;
     }
 
